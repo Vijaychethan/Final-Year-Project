13bc5b6e90e66cdd6b2c3cd44d859a0252c55fb7
Vijaychethan <105622022+Vijaychethan@users.noreply.github.com>
2024-03-20 09:58:18 +0530

    Delete UI directory

--- UI/old/main.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-model_path = 'yolov8n-seg.pt'
-model = YOLO(model_path)
-model.predict(source="C:/Users/VICH/Desktop/Ulcerdetection/train/images/4-Figure3-used_png.rf.5b88ce9292a74bdea14410db5e12b065.jpg",show=True,save=True,hide_labels=False,hide_conf=False,conf=0.5, save_txt=True,save_crop=True,line_thickness=2)
